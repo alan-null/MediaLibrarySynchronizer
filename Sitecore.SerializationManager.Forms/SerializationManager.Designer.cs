84442638b96b9eee83154465e0b5db00185f08ce
Alan NULL <alan-null@outlook.com>
2015-07-05 11:15:11 +0200

    Added menu & context menu.

--- Sitecore.SerializationManager.Forms/SerializationManager.Designer.cs
+++ Sitecore.SerializationManager.Forms/SerializationManager.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.attachToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detachToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.authorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.attachToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.detachToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.downloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.downloadToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -50,29 +65,123 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.listView1);
-            this.splitContainer1.Size = new System.Drawing.Size(605, 314);
+            this.splitContainer1.Size = new System.Drawing.Size(605, 290);
             this.splitContainer1.SplitterDistance = 417;
             this.splitContainer1.TabIndex = 0;
-            // 
-            // listView1
-            // 
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.Location = new System.Drawing.Point(0, 0);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(184, 314);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.List;
             // 
             // treeView1
             // 
             this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(417, 314);
+            this.treeView1.Size = new System.Drawing.Size(417, 290);
             this.treeView1.TabIndex = 1;
             this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
             this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(184, 290);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.attachToolStripMenuItem,
+            this.detachToolStripMenuItem,
+            this.downloadToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // attachToolStripMenuItem
+            // 
+            this.attachToolStripMenuItem.Name = "attachToolStripMenuItem";
+            this.attachToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.attachToolStripMenuItem.Text = "Attach";
+            this.attachToolStripMenuItem.Click += new System.EventHandler(this.attachToolStripMenuItem_Click);
+            // 
+            // detachToolStripMenuItem
+            // 
+            this.detachToolStripMenuItem.Name = "detachToolStripMenuItem";
+            this.detachToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.detachToolStripMenuItem.Text = "Detach";
+            this.detachToolStripMenuItem.Click += new System.EventHandler(this.detachToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.authorToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // authorToolStripMenuItem
+            // 
+            this.authorToolStripMenuItem.Name = "authorToolStripMenuItem";
+            this.authorToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.authorToolStripMenuItem.Text = "Author";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(605, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.attachToolStripMenuItem1,
+            this.detachToolStripMenuItem1,
+            this.downloadToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 92);
+            // 
+            // attachToolStripMenuItem1
+            // 
+            this.attachToolStripMenuItem1.Name = "attachToolStripMenuItem1";
+            this.attachToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.attachToolStripMenuItem1.Text = "Attach";
+            this.attachToolStripMenuItem1.Click += new System.EventHandler(this.attachToolStripMenuItem1_Click);
+            // 
+            // detachToolStripMenuItem1
+            // 
+            this.detachToolStripMenuItem1.Name = "detachToolStripMenuItem1";
+            this.detachToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.detachToolStripMenuItem1.Text = "Detach";
+            this.detachToolStripMenuItem1.Click += new System.EventHandler(this.detachToolStripMenuItem1_Click);
+            // 
+            // downloadToolStripMenuItem
+            // 
+            this.downloadToolStripMenuItem.Name = "downloadToolStripMenuItem";
+            this.downloadToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.downloadToolStripMenuItem.Text = "Download";
+            this.downloadToolStripMenuItem.Click += new System.EventHandler(this.downloadToolStripMenuItem_Click);
+            // 
+            // downloadToolStripMenuItem1
+            // 
+            this.downloadToolStripMenuItem1.Name = "downloadToolStripMenuItem1";
+            this.downloadToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.downloadToolStripMenuItem1.Text = "Download";
+            this.downloadToolStripMenuItem1.Click += new System.EventHandler(this.downloadToolStripMenuItem1_Click);
             // 
             // SerializationManager
             // 
@@ -80,6 +189,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(605, 314);
             this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "SerializationManager";
             this.Text = "SerializationManager";
             this.Load += new System.EventHandler(this.SerializationManager_Load);
@@ -87,7 +198,11 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -96,6 +211,18 @@
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem attachToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detachToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem authorToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem attachToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem detachToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem downloadToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem downloadToolStripMenuItem;
     }
 }
 
--- Sitecore.SerializationManager.Forms/SerializationManager.cs
+++ Sitecore.SerializationManager.Forms/SerializationManager.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Sitecore.SerializationManager.Forms
 {
     public partial class SerializationManager : Form
     {
+        public Manager Manager { get; set; }
+
         public SerializationManager()
         {
             InitializeComponent();
+            Manager = new Manager();
         }
 
         private void SerializationManager_Load(object sender, EventArgs e)
@@ -30,7 +26,7 @@ namespace Sitecore.SerializationManager.Forms
             }
         }
 
-        void FillChildNodes(TreeNode node)
+        private void FillChildNodes(TreeNode node)
         {
             try
             {
@@ -56,7 +52,7 @@ namespace Sitecore.SerializationManager.Forms
                 DirectoryInfo dirs = new DirectoryInfo(node.FullPath);
                 foreach (var file in dirs.GetFiles())
                 {
-                    listView1.Items.Add(file.Name, file.Name);
+                    listView1.Items.Add(file.Name, file.Name, 0);
                 }
             }
             catch (Exception ex)
@@ -78,6 +74,89 @@ namespace Sitecore.SerializationManager.Forms
         private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
         {
             FillListView(e.Node);
+        }
+
+        private void attachToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AttachFileCommand();
+        }
+
+        private void detachToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DetachFileCommand();
+        }
+
+
+        private void downloadToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            DownloadFileCommand();
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (listView1.FocusedItem.Bounds.Contains(e.Location) == true)
+                {
+                    contextMenuStrip1.Show(Cursor.Position);
+                }
+            }
+        }
+
+        private void attachToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            AttachFileCommand();
+        }
+
+        private void detachToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            DetachFileCommand();
+        }
+
+        private void downloadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DownloadFileCommand();
+        }
+
+        private void AttachFileCommand()
+        {
+            var itemName = listView1.SelectedItems;
+            if (itemName.Count > 0)
+            {
+                var itemPath = String.Format("{0}\\{1}", treeView1.SelectedNode.FullPath, itemName[0].Name);
+                if (IsValidFile(itemPath))
+                {
+                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        string filePath = openFileDialog1.FileName;
+                        Manager.AttachFileToSerializationItem(itemPath, filePath);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(@"Please select '.item' file first");
+            }
+        }
+
+        private void DetachFileCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DownloadFileCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValidFile(string itemPath)
+        {
+            if (new FileInfo(itemPath).Extension != ".item")
+            {
+                MessageBox.Show(@"Cannot attach file to non '.item' file");
+                return false;
+            }
+            return true;
         }
     }
 }
